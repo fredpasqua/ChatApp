4992095d49c04a6f8fd22869f789a300a2a9d1be
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-07-10 15:58:32 -0400

    add color buttons

--- components/Start.js
+++ components/Start.js
@@ -26,7 +26,11 @@ export default class Screen1 extends React.Component {
         flex: 1,
         padding: 50,
       },
-      button: {},
+      button: {
+        color: "#757083",
+        width: "88%",
+        borderRadius: "0",
+      },
 
       userInterfaceWrapper: {
         flex: 1,
@@ -39,7 +43,7 @@ export default class Screen1 extends React.Component {
       },
       textInput: {
         padding: 10,
-        marginTop: 10,
+        margin: 10,
         height: 40,
         borderColor: "gray",
         borderWidth: 1,
@@ -57,6 +61,43 @@ export default class Screen1 extends React.Component {
         fontWeight: "600",
         textAlign: "center",
       },
+      colorText: {
+        color: "#757083",
+      },
+      colorChoiceWrapper: {
+        flex: 1,
+        width: "88%",
+      },
+      buttonWrapper: {
+        width: "88%",
+        color: "757083",
+        borderColor: "757083",
+        borderWidth: 3,
+        borderRadius: 5,
+      },
+      colorPicker: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        marginTop: 20,
+        marginBottom: 20,
+      },
+      circle: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+      },
+      color1: {
+        backgroundColor: "#090C08",
+      },
+      color2: {
+        backgroundColor: "#474056",
+      },
+      color3: {
+        backgroundColor: "#8A95A5",
+      },
+      color4: {
+        backgroundColor: "#B9C6AE",
+      },
     });
 
     return (
@@ -65,6 +106,7 @@ export default class Screen1 extends React.Component {
           <View style={styles.titleWrapper}>
             <Text style={styles.titleText}>Blather</Text>
           </View>
+
           <View style={styles.userInterfaceWrapper}>
             <TextInput
               style={styles.textInput}
@@ -72,11 +114,26 @@ export default class Screen1 extends React.Component {
               value={this.state.name}
               placeholder="Your name:"
             />
-            <Button
-              style={styles.button}
-              title="Start Chatting"
-              onPress={() => this.props.navigation.navigate("Chat")}
-            />
+
+            <View style={styles.colorChoiceWrapper}>
+              <Text style={styles.circle.colorText}>
+                Choose Background Color:
+              </Text>
+              <View style={styles.colorPicker}>
+                <View style={[styles.circle, styles.color1]}></View>
+                <View style={[styles.circle, styles.color2]}> </View>
+                <View style={[styles.circle, styles.color3]}></View>
+                <View style={[styles.circle, styles.color4]}></View>
+              </View>
+            </View>
+
+            <View style={styles.buttonWrapper}>
+              <Button
+                style={styles.button}
+                title="Start Chatting"
+                onPress={() => this.props.navigation.navigate("Chat")}
+              />
+            </View>
           </View>
         </ImageBackground>
       </View>
