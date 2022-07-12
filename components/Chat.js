2f1da3c8918c5518cd09859a400e181b8b083ee0
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-07-11 21:22:37 -0400

    add function to set navigation Name to user name

--- components/Chat.js
+++ components/Chat.js
@@ -2,7 +2,12 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export default class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    let { name } = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Chat Screen</Text>
@@ -10,3 +15,4 @@ export default class Chat extends React.Component {
     );
   }
 }
+
