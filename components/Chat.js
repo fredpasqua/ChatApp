bb2ff535c2a23fed66480921877d8f66c80e53f1
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-07-12 14:57:39 -0400

    updateds

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,49 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import { GiftedChat } from "react-native-gifted-chat";
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: [],
+    };
   }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     let { name, color } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: color,
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
         }}
-      >
-        <Text>Chat Screen</Text>
-      </View>
+      />
     );
   }
 }
