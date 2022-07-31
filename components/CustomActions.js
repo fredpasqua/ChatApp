19ef43c849f674b53641f70f77c554896a9c3253
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-07-31 11:17:45 -0400

    Add function to customActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,7 +1,119 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+import firebase from "firebase";
+import firestore from "firebase";
 
 export class CustomActions extends Component {
+  /**
+   * Let the user pick an image from the device's image library
+   * @function imagePicker
+   * @async
+   */
+  imagePicker = async () => {
+    // expo permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    try {
+      if (status === "granted") {
+        // pick image
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images, // only images are allowed
+        }).catch((error) => console.log(error));
+        // canceled process
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  /**
+   * Let the user take a photo with device's camera
+   * @function takePhoto
+   * @async
+   */
+  takePhoto = async () => {
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.CAMERA_ROLL
+    );
+    try {
+      if (status === "granted") {
+        const result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => console.log(error));
+
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  /**
+   * get the location of the user by using GPS
+   * @function getLocation
+   * @async
+   */
+  getLocation = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === "granted") {
+        const result = await Location.getCurrentPositionAsync({}).catch(
+          (error) => console.log(error)
+        );
+        const longitude = JSON.stringify(result.coords.longitude);
+        const latitude = JSON.stringify(result.coords.latitude);
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  onActionPress = () => {
+    const options = [
+      "Choose From Library",
+      "Take Picture",
+      "Send Location",
+      "Cancel",
+    ];
+    const cancelButtonIndex = options.length - 1;
+    this.context.actionSheet().showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            console.log("user wants to pick an image");
+            return this.imagePicker();
+          case 1:
+            console.log("user wants to take a photo");
+            return this.takePhoto();
+          case 2:
+            console.log("user wants to get their location");
+            return this.getLocation();
+        }
+      }
+    );
+  };
   render() {
     return (
       <TouchableOpacity style={[styles.container]} onPress={this.onActionPress}>
@@ -33,5 +145,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func,
+};
 
 export default CustomActions;
